16a086cfd4b7724df647150d4dc9323eca9a30c4
Byron Seto <byronseto@hotmail.com>
2020-01-14 20:58:05 -0700

    make the project use the go-micro for service detecting

--- consignment-cli/cli.go
+++ consignment-cli/cli.go
@@ -10,7 +10,7 @@ import (
 	"context"
 
 	pb "github.com/bseto/microservice-example/consignment-service/proto/consignment"
-	"google.golang.org/grpc"
+	"github.com/micro/go-micro"
 )
 
 const (
@@ -29,13 +29,11 @@ func parseFile(file string) (*pb.Consignment, error) {
 }
 
 func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Did not connect: %v", err)
-	}
-	defer conn.Close()
-	client := pb.NewShippingServiceClient(conn)
+
+	service := micro.NewService(micro.Name("shippy.consignment.cli"))
+	service.Init()
+
+	client := pb.NewShippingServiceClient("shippy.service.consignment", service.Client())
 
 	// Contact the server and print out its response.
 	file := defaultFilename
--- consignment-cli/go.mod
+++ consignment-cli/go.mod
@@ -3,7 +3,8 @@ module github.com/bseto/microservice-example/consignment-cli
 go 1.13
 
 require (
-	github.com/bseto/microservice-example/consignment-service v0.0.0-20200108033700-8ef4bc2d3513
+	github.com/bseto/microservice-example/consignment-service v0.0.0-20200108045719-2393dca0f736
+	github.com/micro/go-micro v1.18.0
 	github.com/micro/protobuf v0.0.0-20180321161605-ebd3be6d4fdb // indirect
 	google.golang.org/grpc v1.26.0
 )
